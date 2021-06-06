29b447e7fd349dc1356bf2494caac4dd3452c139
Shade <73481203+Shade0122@users.noreply.github.com>
2021-06-06 13:00:22 +0100

    Add files via upload

---
+++ ShadesSpecChecker/Class.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Management;
+using System.Net;
+using System.Text;
+
+namespace ShadesSpecChecker
+{
+    class Class
+    {
+        public static StringBuilder String = new StringBuilder();
+        public static void MachineGuid()
+        {
+            if (RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64).OpenSubKey(@"SOFTWARE\Microsoft\Cryptography") == null)
+                throw new KeyNotFoundException(string.Format("Registry key not found: {0}", @"SOFTWARE\Microsoft\Cryptography"));
+            if (RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64).OpenSubKey(@"SOFTWARE\Microsoft\Cryptography").GetValue("MachineGuid") == null)
+                throw new IndexOutOfRangeException(string.Format("Index not found: {0}", "MachineGuid"));
+            WriteLine(RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64).OpenSubKey(@"SOFTWARE\Microsoft\Cryptography").GetValue("MachineGuid").ToString());
+        }
+
+        public static void IP()
+        {
+            WriteLine(new WebClient().DownloadString("http://ipv4bot.whatismyipaddress.com/"));
+        }
+
+        public static void Win32Component(string Class, string Syntax) //https://docs.microsoft.com/en-gb/windows/win32/cimwin32prov/computer-system-hardware-classes?redirectedfrom=MSDN
+        {
+            foreach (ManagementObject managementObject in new ManagementObjectSearcher("root\\CIMV2", "Select * FROM " + Class).Get())
+            {
+                WriteLine(Convert.ToString(managementObject[Syntax].ToString()));
+            }
+        }
+
+        public static void WriteLine(string _string)
+        {
+            Console.WriteLine(_string);
+            String.Append(_string).Append(Environment.NewLine);
+        }
+
+        public static void Write(string _string)
+        {
+            Console.Write(_string);
+            String.Append(_string);
+        }
+
+        public static void Log()
+        {
+            try
+            {
+                using (StreamWriter File = System.IO.File.AppendText("./Log.txt"))
+                {
+                    File.Write(String.ToString());
+                    File.Close();
+                    File.Dispose();
+                }
+            } 
+            catch (Exception exception)
+            {
+                WriteLine("Failed to write file, exception caught: " + exception);
+            }
+        }
+    }
+}
---
+++ ShadesSpecChecker/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ShadesSpecChecker
+{
+    class Program
+    {
+        static void Main()
+        {
+            Class.WriteLine("=========================== " + DateTime.Now.ToString() + " =========================== By Shade#0122" + "\n");
+
+            Class.WriteLine("Computer Sytem Hardware: ");
+            Class.Write("Processor: "); Class.Win32Component("Win32_Processor", "Name");
+            Class.Write("USB Controller: "); Class.Win32Component("Win32_USBController", "Name");
+            Class.Write("Battery: "); Class.Win32Component("Win32_Battery", "Name");
+            Class.Write("Voltage Probe: "); Class.Win32Component("Win32_VoltageProbe", "Name");
+            Class.Write("Desktop Monitor: "); Class.Win32Component("Win32_DesktopMonitor", "Name");
+            Class.Write("Video Controller: "); Class.Win32Component("Win32_VideoController", "Name");
+
+            Class.WriteLine("\n" + "Computer Information:");
+            Class.Write("Machine Guid: "); Class.MachineGuid();
+            Class.Write("IP: "); Class.IP();
+            Class.Write("User Name: " + Environment.UserName + "\n");
+            Class.Write("Machine Name: " + Environment.MachineName + "\n");
+
+            Class.WriteLine("\n" + "Information was last logged: " + DateTime.Now.ToString());
+
+            Class.Log();
+
+            Console.WriteLine("\n" + "Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
